1dfb25c81a8cbb84c986cee7e6d8e4196423ef26
Ryan-yang125 <ruiyang0012@gmail.com>
2024-07-13 17:08:23 +0800

    feat: analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,15 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-
+import vercel from "@astrojs/vercel/serverless";
 import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  site: process.env.CI ? "https://bio.ruiy.sh" : "http://localhost:4321",
+  // site: process.env.CI ? "https://bio.ruiy.sh" : "http://localhost:4321",
+  site: "https://bio.ruiy.sh",
   integrations: [tailwind(), react()],
+  output: "server",
+  adapter: vercel({
+    webAnalytics: { enabled: true },
+  }),
 });
